4fa2d996496e9f427c11e320dd55305c7653e9fe
Denis Ramazanov <denkord415@gmail.com>
2024-09-24 21:20:45 +0300

    desktop

--- script.js
+++ script.js
@@ -1,3 +1,82 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    
+});
+
+const TourneySwiper = new Swiper('.tourney__swiper',{
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.tourney__swiper-button-next',
+      prevEl: '.tourney__swiper-button-prev',
+    },
+  
+})
+
+const ProjectsSwiper = new Swiper('.projects__swiper',{
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  initialSlide: 1,
+  spaceBetween: 40,
+  centeredSlides: true,
+  centerInsufficientSlides: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.projects__swiper-button-next',
+    prevEl: '.projects__swiper-button-prev',
+  },
+
+})
+
+const MedicineSwiper = new Swiper('.medicine__swiper',{
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: 2,
+  spaceBetween: 140,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  scrollbar: {
+    el: '.wrapper__scrollbar',
+    draggable: true,
+    dragSize: 270,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.medicine__swiper-button-next',
+    prevEl: '.medicine__swiper-button-prev',
+  },
+
+})
+
+let header = document.querySelector(".header")
+let logo = document.querySelector(".logo");
 let burger = document.querySelector(".burger");
 let wrapperBurger = document.querySelector(".wrapper__burger");
 let circle = document.querySelector(".circle");
@@ -5,11 +84,7 @@ let headerLinks = document.querySelector(".header__links")
 let search = document.querySelector(".search")
 let nav = document.querySelector(".wrapper__nav")
 let body = document.querySelector(".body")
-let arrowLeft = document.getElementById("arrow__left")
-let arrowRight = document.getElementById("arrow__right")
-let tabs = document.getElementById("tabs")
-let slides = document.querySelectorAll(".slide__events-wrapper")
-// let slider = document.querySelector(".frime__events-content")
+let navLink = document.querySelectorAll(".nav__link")
 
 wrapperBurger.addEventListener("click", ()=>{
     burger.classList.toggle("active")
@@ -21,79 +96,14 @@ wrapperBurger.addEventListener("click", ()=>{
     body.classList.toggle("active")
 })
 
-
-// let currentSlideIndex = 0;
-// const paginationCircles = [];
-// const sliderWidth = slider.clientWidth;
-
-// function createPaginationCircle() {
-//     const div = document.createElement("div");
-//     div.className = "tab";
-//     tabs.appendChild(div);
-//     paginationCircles.push(div);
-// }
-
-let currentSlideIndex = 0;
-const paginationCircles = [];
-
-function createPaginationCircle() {
-    const div = document.createElement("div");
-    div.className = "tab";
-    tabs.appendChild(div);
-    paginationCircles.push(div);
-}
-
-function addPagination() {
-    slides.forEach(createPaginationCircle);
-    paginationCircles[0].classList.add("active");
-    paginationCircles.forEach((circle, index) => {
-        circle.addEventListener("click", () => changeSlide(index));
-    });
-}
-
-function addActiveClass() {
-    paginationCircles[currentSlideIndex].classList.add("active");
-}
-
-function removeActiveClass() {
-    paginationCircles[currentSlideIndex].classList.remove("active");
-}
-
-function showSlide() {
-    slides[currentSlideIndex].classList.add("block");
-}
-
-function hideSlide() {
-    slides[currentSlideIndex].classList.remove("block");
-}
-
-function changeSlide(slideIndex) {
-    hideSlide();
-    removeActiveClass();
-    currentSlideIndex = slideIndex;
-    addActiveClass();
-    showSlide();
-}
-
-function nextSlide() {
-    let newSlideIndex = currentSlideIndex + 1;
-    if(newSlideIndex > slides.length - 1) {
-        arrowRight.disabled
-    } 
-    else {
-        arrowRight.disabled = false
-    }
-    changeSlide(newSlideIndex);
-}
-
-function previousSlide() {
-    let newSlideIndex = currentSlideIndex - 1;
-    if(newSlideIndex < 0) {
-        // newSlideIndex = slides.length - 1;
-    }
-    changeSlide(newSlideIndex);
-}
-
-addPagination();
-arrowLeft.addEventListener("click", previousSlide);
-arrowRight.addEventListener("click", nextSlide);
+navLink.forEach(link => {
+  link.addEventListener("click", ()=>{
+    burger.classList.toggle("active")
+    wrapperBurger.classList.toggle("active")
+    circle.classList.toggle("active")
+    headerLinks.classList.toggle("active")
+    search.classList.toggle("active")
+    nav.classList.toggle("active")
+    body.classList.toggle("active")
+  })
+});
