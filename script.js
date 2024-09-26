645578acfac4c23f8c03ad7490cfe5b00ad0a0b3
Denis Ramazanov <denkord415@gmail.com>
2024-09-26 09:35:06 +0300

    color

--- script.js
+++ script.js
@@ -85,6 +85,14 @@ let search = document.querySelector(".search")
 let nav = document.querySelector(".wrapper__nav")
 let body = document.querySelector(".body")
 let navLink = document.querySelectorAll(".nav__link")
+let eventsBlock = document.querySelector(".header__line-events")
+let countriesBlock = document.querySelector(".countries")
+let targetBlock = document.querySelector(".target")
+let VKIcon = document.querySelector(".VK__icon")
+let TGIcon = document.querySelector(".TG__icon")
+let langRu = document.querySelector(".lang__ru")
+let langEng = document.querySelector(".lang__eng")
+
 
 wrapperBurger.addEventListener("click", ()=>{
     burger.classList.toggle("active")
@@ -94,6 +102,9 @@ wrapperBurger.addEventListener("click", ()=>{
     search.classList.toggle("active")
     nav.classList.toggle("active")
     body.classList.toggle("active")
+    langRu.classList.toggle("active__color")
+    langEng.classList.toggle("active__color")
+    logo.classList.toggle("active__color")
 })
 
 navLink.forEach(link => {
@@ -107,3 +118,46 @@ navLink.forEach(link => {
     body.classList.toggle("active")
   })
 });
+
+window.addEventListener('scroll', ()=>{
+  let eventsBlockPosition = eventsBlock.getBoundingClientRect()
+  let countriesBlockPosition = countriesBlock.getBoundingClientRect()
+  let targetBlockPosition = targetBlock.getBoundingClientRect()
+  let logoPosition = logo.getBoundingClientRect()
+  let burgerPostition = burger.getBoundingClientRect()
+  let VKIconPosition = VKIcon.getBoundingClientRect()
+  let TGIconPosition = TGIcon.getBoundingClientRect()
+  let langRuPosition = langRu.getBoundingClientRect()
+  let langEngPosition = langEng.getBoundingClientRect()
+  console.log(langRuPosition)
+  if (((logoPosition.top >= eventsBlockPosition.top) && (logoPosition.top < eventsBlockPosition.bottom)) || (logoPosition.top >= countriesBlockPosition.top) && (logoPosition.top <= targetBlockPosition.bottom)) {
+    logo.classList.add("active")
+  } else {
+    logo.classList.remove("active")
+  }
+  if (((burgerPostition.top >= eventsBlockPosition.top) && (burgerPostition.top <= eventsBlockPosition.bottom)) || (burgerPostition.top >= countriesBlockPosition.top) && (burgerPostition.top <= targetBlockPosition.bottom)) {
+    burger.classList.add("color")
+  } else {
+    burger.classList.remove("color")
+  }
+  if (((VKIconPosition.top >= eventsBlockPosition.top) && (VKIconPosition.top <= eventsBlockPosition.bottom)) || (VKIconPosition.top >= countriesBlockPosition.top) && (VKIconPosition.top <= targetBlockPosition.bottom)) {
+    VKIcon.classList.add("color")
+  } else {
+    VKIcon.classList.remove("color")
+  }
+  if (((TGIconPosition.top >= eventsBlockPosition.top) && (TGIconPosition.top <= eventsBlockPosition.bottom)) || (TGIconPosition.top >= countriesBlockPosition.top) && (TGIconPosition.top <= targetBlockPosition.bottom)) {
+    TGIcon.classList.add("color")
+  } else {
+    TGIcon.classList.remove("color")
+  }
+  if (((langRuPosition.top >= eventsBlockPosition.top) && (langRuPosition.top <= eventsBlockPosition.bottom)) || (langRuPosition.top >= countriesBlockPosition.top) && (langRuPosition.top <= targetBlockPosition.bottom)) {
+    langRu.classList.add("color")
+  } else {
+    langRu.classList.remove("color")
+  }
+  if (((langEngPosition.top >= eventsBlockPosition.top) && (langEngPosition.top <= eventsBlockPosition.bottom)) || (langEngPosition.top >= countriesBlockPosition.top) && (langEngPosition.top <= targetBlockPosition.bottom)) {
+    langEng.classList.add("color")
+  } else {
+    langEng.classList.remove("color")
+  }
+})
